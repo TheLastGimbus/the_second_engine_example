5f0e223e726586cb22e680c69da94b9200108e79
TheLastGimbus <mateusz.soszynski@tuta.io>
2025-02-21 23:03:02 +0100

    some jni

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,39 @@
+import 'dart:isolate';
+
 import 'package:flutter/material.dart';
+import 'package:jni/jni.dart';
+import 'package:the_second_engine_example/toast.g.dart' as jni;
+import 'package:workmanager/workmanager.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Workmanager().initialize(
+    callbackDispatcher,
+    isInDebugMode: false,
+  );
   runApp(const MyApp());
+}
+
+@pragma('vm:entry-point')
+void callbackDispatcher() {
+  Workmanager().executeTask((task, inputData) async {
+    print(
+        'BGN: waiting 15 seconds (open app now!) (Isolate ${Isolate.current.hashCode}');
+    final ctx = jni.Context.fromReference(Jni.getCachedApplicationContext());
+    final manager = ctx
+        .getSystemService(jni.Context.BLUETOOTH_SERVICE)!
+        .as(jni.BluetoothManager.type, releaseOriginal: true);
+    final adapter = manager.getAdapter()!;
+    await Stream.periodic(Duration(seconds: 1), (count) {
+      print(
+          'BGN🥸: bluetooth enabled: ${adapter.isEnabled()} (Isolate ${Isolate.current.hashCode})');
+    })
+        .listen((i) {})
+        .asFuture()
+        .timeout(Duration(seconds: 15), onTimeout: () {});
+    print('BGN: bye bye');
+    return true;
+  });
 }
 
 class MyApp extends StatelessWidget {
@@ -15,7 +47,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'the second2️⃣ engine'),
     );
   }
 }
@@ -31,8 +63,23 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  late final jni.Context ctx;
+  late final jni.BluetoothManager manager;
+  late final jni.BluetoothAdapter adapter;
+
+  @override
+  void initState() {
+    final ctx = jni.Context.fromReference(Jni.getCachedApplicationContext());
+    manager = ctx
+        .getSystemService(jni.Context.BLUETOOTH_SERVICE)!
+        .as(jni.BluetoothManager.type, releaseOriginal: true);
+    adapter = manager.getAdapter()!;
+    super.initState();
+  }
 
   void _incrementCounter() {
+    print(
+        'APP📱: bluetooth enabled: ${adapter.isEnabled()} (Isolate ${Isolate.current.hashCode})');
     setState(() {
       _counter++;
     });
@@ -43,14 +90,24 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
         title: Text(widget.title),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+            FilledButton(
+              onPressed: () {
+                Workmanager().registerOneOffTask(
+                  'uniqueName',
+                  'taskName',
+                  initialDelay: Duration(seconds: 5),
+                );
+                print('will run in 5 seconds...');
+              },
+              child: Text("one off"),
+            ),
+            const Text('You crashed flutter this many times:'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
